2963e1288b1316ad9a5cda316cf5219fd2cf13bf
FANGong <87745513+naFgG@users.noreply.github.com>
2023-07-25 13:13:37 +0800

    Update rda_cca.R

--- rda_cca.R
+++ rda_cca.R
@@ -2,15 +2,17 @@ rm(list = ls())
 library(vegan)
 library(ggplot2)
 
+args <- commandArgs(T)
+
 mycol <- c("#7FC97F","#BEAED4","#FDC086","#386CB0","#F0027F","#BF5B17",
            "#666666","#1B9E77","#7570B3","#66A61E", "#E6AB02","#A6761D")
 
 # 列为样本，行为物种
-genus <- read.table('genus.top10.xls', header=TRUE, row.names=1, sep="\t")
-mapping <- read.table('mapping.txt', header=TRUE, sep="\t")
+genus <- read.table(args[1], header=TRUE, row.names=1, sep="\t")
+mapping <- read.table(args[2], header=TRUE, sep="\t")
 genus <- t(genus)
 # 列为环境因子，行为样本
-env <- read.table('pc.xls', row.names=1, sep='\t', header=TRUE,
+env <- read.table(args[3], row.names=1, sep='\t', header=TRUE,
                   stringsAsFactors=FALSE, check.names=FALSE)
 # 调整环境因子中样品的顺序
 env <- env[match(row.names(genus), row.names(env)),]
