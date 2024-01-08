a177b694508a6a16a8c49757c51ae2b6361efcd3
FANGong <87745513+naFgG@users.noreply.github.com>
2024-01-08 11:06:12 +0800

    Update diff_DESeq2.R

--- diff_DESeq2.R
+++ diff_DESeq2.R
@@ -4,15 +4,13 @@ library(ggplot2)
 library(ggrepel)
 library(tidyverse)
 
-countdata <- read.table("genus/genus.xls", header=T, row.names=1, check.names=F)
-# Abundance top 30
-# top30 <- countdata %>% rowSums() %>% sort(decreasing=T) %>% head(30) %>% names()
-# countdata <- countdata[top30, ]
-
-coldata <- read.table("mapping.txt", header=T, row.names=1, check.names=F)
+# counts file
+countdata <- read.table("", header=T, row.names=1, check.names=F)
+# metadata
+coldata <- read.table("", header=T, row.names=1, check.names=F)
 countdata <- countdata[, rownames(coldata)]
 coldata$Group <- factor(coldata$Group, levels=unique(coldata$Group))
-
+# if counts file are not int, run this for-loop
 for (i in rownames(countdata)){
   for (j in colnames(countdata)){
     countdata[i, j] <- round(countdata[i, j], digits=0)
@@ -20,15 +18,14 @@ for (i in rownames(countdata)){
 }
 
 dds <- DESeqDataSetFromMatrix(countData=countdata, colData=coldata, design=~ Group)
-# 一键式差异分析
+# One click differential analysis
 dds2 <- DESeq(dds)
 # res <- results(dds2, contrast=c("Group", levels(coldata$Group)[1], levels(coldata$Group)[2]))
-# 默认 对照 vs. 控制
+# default: Treat vs. Control
 res <- results(dds2)
-# openxlsx::write.xlsx(as.data.frame(res), "genus/diff_genus_top30.xlsx", rowNames=T)
-openxlsx::write.xlsx(as.data.frame(res), "genus/diff_genus.xlsx", rowNames=T)
+openxlsx::write.xlsx(as.data.frame(res), ".xlsx", rowNames=T)
 
-# 画图
+# Vocalno plot
 df <- as.data.frame(res[, c("log2FoldChange", "pvalue", "padj")])
 for (i in rownames(df)){
   if (is.na(df[i, "padj"])){
@@ -43,11 +40,6 @@ for (i in rownames(df)){
   }
 }
 df$pinfo <- factor(df$pinfo, levels=c("padj", "p", "non"))
-
-# padj < 0.05 min30
-df2 <- df %>% arrange(padj) %>% head(15)
-# all p
-# df2 <- subset(df, pvalue < 0.05)
 
 p <- ggplot(df, aes(x=log2FoldChange, y=-log10(pvalue), color=pinfo, shape=pinfo, fill=pinfo)) +
   geom_point()+
@@ -68,10 +60,8 @@ p <- ggplot(df, aes(x=log2FoldChange, y=-log10(pvalue), color=pinfo, shape=pinfo
                     labels=c(expression("Adjusted "*italic(P)*" < 0.05"), expression(italic(P)*" < 0.05"), '')) +
   geom_text_repel(data=df2, aes(label=rownames(df2)), 
                   color="black", size=3, nudge_y=0.01) + 
- # scale_y_continuous(breaks=seq(0, 25, by=5), expand=c(0.01, 0.05)) +
   guides(color=guide_legend(override.aes=list(shape=c(16, 1, NA), size=3)))
 
-# pdf("genus/volcano_top30.pdf")
-pdf("genus/volcano_pmin15.pdf")
+pdf(".pdf")
 p
 dev.off()
